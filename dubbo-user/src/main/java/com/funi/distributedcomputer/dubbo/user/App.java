c6aca0ee5ae439156fc348bea9227251ce5a63cf
zengshunyao <641995773@qq.com>
2018-07-24 22:02:09 +0800

    无zookeeper rpc调用

--- dubbo-user/src/main/java/com/funi/distributedcomputer/dubbo/user/App.java
+++ dubbo-user/src/main/java/com/funi/distributedcomputer/dubbo/user/App.java
@@ -3,14 +3,18 @@ package com.funi.distributedcomputer.dubbo.user;
 import com.funi.distributedcomputer.dubbo.order.DoOrderRequest;
 import com.funi.distributedcomputer.dubbo.order.DoOrderResponse;
 import com.funi.distributedcomputer.dubbo.order.IOrderService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("order-consumer.xml");
+
         System.out.println("Hello World!");
-        IOrderService orderService = null;
+        IOrderService orderService = applicationContext.getBean("orderService", IOrderService.class);
 
         DoOrderRequest request = new DoOrderRequest();
         request.setName("bug");
