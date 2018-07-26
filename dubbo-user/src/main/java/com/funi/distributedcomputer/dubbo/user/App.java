35d765e0853793a06efa050b2a4d932fdb8207e9
shunyao.zeng <shunyao.zeng@mail.funi365.com>
2018-07-26 14:27:06 +0800

    异步

--- dubbo-user/src/main/java/com/funi/distributedcomputer/dubbo/user/App.java
+++ dubbo-user/src/main/java/com/funi/distributedcomputer/dubbo/user/App.java
@@ -1,17 +1,19 @@
 package com.funi.distributedcomputer.dubbo.user;
 
+import com.alibaba.dubbo.rpc.RpcContext;
 import com.funi.distributedcomputer.dubbo.order.DoOrderRequest;
 import com.funi.distributedcomputer.dubbo.order.DoOrderResponse;
 import com.funi.distributedcomputer.dubbo.order.IOrderService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("order-consumer.xml");
 
@@ -24,7 +26,11 @@ public class App {
         DoOrderResponse response = orderService.doOrder(request);
         System.out.println(response);
 
-        DoOrderResponse response2 = orderService2.doOrder(request);
+        DoOrderResponse response2 = null;
+        orderService2.doOrder(request);
+        Future<DoOrderResponse> future = RpcContext.getContext().getFuture();
+        System.out.println("---阻赛中......----");
+        response2 = future.get();
         System.out.println(response2);
 
 //        try {
